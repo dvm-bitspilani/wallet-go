e761e4dba509e7ccfb58bf251a2069bb10a03484
Harsh Singh <harshsingh050803@gmail.com>
2023-01-14 06:29:19 +0530

    refactor: change userService to return a status code as well

--- service/userService.go
+++ service/userService.go
@@ -45,7 +45,7 @@ func (r *UserOps) GetOrCreateWallet(user *ent.User) (*ent.Wallet, error) {
 	if !user.Disabled {
 		err := fmt.Errorf("cannot create or return wallet of as %s is disabled", user.Username)
 		//err := exceptions.UserDisabledException{Exception: exceptions.Exception{Message: fmt.Sprintf("cannot create or return wallet of as %s is disabled", user.Username)}}
-		return nil, err
+		return nil, err //403
 	}
 	wallet, err := user.QueryWallet().Only(r.ctx)
 	if err != nil {
@@ -54,21 +54,21 @@ func (r *UserOps) GetOrCreateWallet(user *ent.User) (*ent.Wallet, error) {
 	return wallet, nil
 }
 
-func (r *UserOps) Transfer(user *ent.User, target *ent.User, amount int) (*ent.Transactions, error) {
+func (r *UserOps) Transfer(user *ent.User, target *ent.User, amount int) (*ent.Transactions, error, int) {
 	if user.Disabled {
 		err := fmt.Errorf("requesting User is disabled")
 		//err := exceptions.UserDisabledException{Exception: exceptions.Exception{Message: "requesting User is disabled"}}
-		return nil, err
+		return nil, err, 403 //403
 	}
 	if target.Disabled {
 		err := fmt.Errorf("target User is disabled")
 		//err := exceptions.UserDisabledException{Exception: exceptions.Exception{Message: "target User is disabled"}}
-		return nil, err
+		return nil, err, 403 //403
 	}
 	if amount <= 0 {
 		err := fmt.Errorf("amount cannot be negative")
 		//err := exceptions.Exception{Message: "amount cannot be negative", Status: 400}
-		return nil, err
+		return nil, err, 400 //400
 	}
 	occupationPair := []string{user.Occupation.String(), target.Occupation.String()}
 	validOccupationPair := false // It's probably a good idea to prevent any transaction than to allow *any* transaction
@@ -79,60 +79,62 @@ func (r *UserOps) Transfer(user *ent.User, target *ent.User, amount int) (*ent.T
 		}
 	}
 	if validOccupationPair {
-		transaction, err := GenerateAndPerform(amount, helpers.TRANSFER, user, target, r.ctx, r.client)
+		transaction, err, statusCode := GenerateAndPerform(amount, helpers.TRANSFER, user, target, r.ctx, r.client)
 		if err != nil {
-			return nil, err
+			return nil, err, statusCode
 		}
-		return transaction, nil
+		return transaction, nil, 0
 	} else {
 		err := fmt.Errorf("transaction forbidden: %s", occupationPair)
 		//err := exceptions.Exception{Message: fmt.Sprintf("transaction forbidden: %s", occupationPair), Status: 403}
-		return nil, err
+		return nil, err, 403 //403
 	}
 }
 
-func (r *UserOps) PlaceOrder(usr *ent.User, orderList []helpers.OrderActionVendorStruct) (*OrderShellStruct, error) {
+func (r *UserOps) PlaceOrder(usr *ent.User, orderList []helpers.OrderActionVendorStruct) (*OrderShellStruct, error, int) {
+	var statusCode int
+
 	if !validator.In(usr.Occupation, "bitsian", "participant") {
-		return nil, errors.New("only bitsians and participants may place orders")
+		return nil, errors.New("only bitsians and participants may place orders"), 403
 	}
 	var totalPrice int
 	if usr.Disabled {
-		return nil, errors.New("cannot place order, user's account has been disabled")
+		return nil, errors.New("cannot place order, user's account has been disabled"), 403
 	}
 	// TODO:	refactor this so we're not running two for loops
 	for _, vendorStruct := range orderList {
 		vendorObj, err := r.client.VendorSchema.Query().Where(vendor.ID(vendorStruct.VendorId)).Only(r.ctx)
 		if err != nil {
-			return nil, fmt.Errorf("vendor with ID %d does not exist", vendorStruct.VendorId)
+			return nil, fmt.Errorf("vendor with ID %d does not exist", vendorStruct.VendorId), 403 //404
 		}
 		if vendorObj.Closed {
-			return nil, fmt.Errorf("Vendor %s is closed", vendorObj.Name)
+			return nil, fmt.Errorf("Vendor %s is closed", vendorObj.Name), 412 //412 ??
 		}
 		if vendorObj.Edges.User.Disabled {
-			return nil, fmt.Errorf("Vendor %s is disabled", vendorObj.Name)
+			return nil, fmt.Errorf("Vendor %s is disabled", vendorObj.Name), 412 // 403
 		}
 
 		for _, itemStruct := range vendorStruct.Order {
 			itemObj, err := r.client.Item.Query().Where(item.ID(itemStruct.ItemId)).Only(r.ctx)
 			if err != nil {
-				return nil, fmt.Errorf("item with ID %d does not exist", itemStruct.ItemId)
+				return nil, fmt.Errorf("item with ID %d does not exist", itemStruct.ItemId), 404
 			}
 			if itemObj.Edges.VendorSchema != vendorObj {
-				return nil, errors.New("cannot order items from the wrong vendor")
+				return nil, errors.New("cannot order items from the wrong vendor"), 403 // 403
 			}
 			if !itemObj.Available {
-				return nil, fmt.Errorf("%s item is currently unavailable", itemObj.Name)
+				return nil, fmt.Errorf("%s item is currently unavailable", itemObj.Name), 412 // 412
 			}
 
 			if itemStruct.Quantity <= 0 {
-				return nil, errors.New("cannot order a negative or 0 quantity of items")
+				return nil, errors.New("cannot order a negative or 0 quantity of items"), 400 // 400
 			}
 			totalPrice += itemObj.BasePrice * itemStruct.Quantity
 		}
 	}
 	walletOps := NewWalletOps(r.ctx, r.client)
 	if totalPrice > walletOps.Balance(usr.Edges.Wallet) {
-		return nil, fmt.Errorf("order price: %d, current balance: %d", totalPrice, walletOps.Balance(usr.Edges.Wallet))
+		return nil, fmt.Errorf("order price: %d, current balance: %d", totalPrice, walletOps.Balance(usr.Edges.Wallet)), 412
 	}
 
 	// creation and saving phase
@@ -156,11 +158,11 @@ func (r *UserOps) PlaceOrder(usr *ent.User, orderList []helpers.OrderActionVendo
 			order.Update().SetPrice(orderOps.CalculateTotalPrice(order)).SaveX(r.ctx)
 		}
 	}
-	err := walletOps.Deduct(usr.Edges.Wallet, totalPrice)
+	err, statusCode := walletOps.Deduct(usr.Edges.Wallet, totalPrice)
 	if err != nil {
-		return nil, err
+		return nil, err, statusCode
 	}
 	//TODO:		put_orders
 	OrderShellOps := NewOrderShellOps(r.ctx, r.client)
-	return OrderShellOps.ToDict(shell), nil
+	return OrderShellOps.ToDict(shell), nil, 0
 }
