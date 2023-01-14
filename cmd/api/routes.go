b50606d7765b69c1376bc2fed1061e17e6a46301
Harsh Singh <harshsingh050803@gmail.com>
2023-01-14 07:02:38 +0530

    feat: wire in all routes for api pkg

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,6 +20,7 @@ func routes(app *config.Application) http.Handler {
 	mux.Use(recoverPanic)
 	mux.Use(authenticate)
 
+	// Health Routes
 	mux.HandleFunc("/status", handlers.Status(app)).Methods("GET")
 
 	// For websockets
@@ -31,12 +32,32 @@ func routes(app *config.Application) http.Handler {
 	mux.HandleFunc("/auth", handlers.Login(app)).Methods("POST")
 
 	authenticatedRoutes := mux.NewRoute().Subrouter()
-
 	requireAuthenticatedUser := newRequireAuthenticatedUser(app)
 	authenticatedRoutes.Use(requireAuthenticatedUser)
 
-	// Montary Routes
-	authenticatedRoutes.HandleFunc("/monetory/add/cash", handlers.AddCash(app)).Methods("POST")
+	// Monetary Routes
+	authenticatedRoutes.HandleFunc("/monetary/add/cash", handlers.AddCash(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/monetary/add/swd", handlers.AddSwd(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/monetary/transfers", handlers.Transfer(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/monetary/getqr", handlers.GetUserQR(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/monetary/balance", handlers.GetBalance(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/monetary/transactions/history", handlers.TransactionHistory(app)).Methods("GET")
+
+	// Ordering Routes
+	authenticatedRoutes.HandleFunc("/order/{shell_id}", handlers.Order(app)).Methods("GET", "POST")
+	authenticatedRoutes.HandleFunc("/order/make_otp_seen", handlers.MakeOtpSeen(app)).Methods("POST")
+
+	// Vendor Routes
+	authenticatedRoutes.HandleFunc("/vendors/", handlers.GetAllVendorsWithMenu(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/vendor/{vendor_id}", handlers.GetVendor(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/vendor/{vendor_id}/items", handlers.GetMenu(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/vendor/orders/{status}", handlers.GetVendorOrders(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/vendor/orders/idlist", handlers.GetOrderIdArrayDetails(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/vendor/order/{order_id}", handlers.GetOrderDetails(app)).Methods("GET")
+	authenticatedRoutes.HandleFunc("/vendor/orders/{order_id}/change_status", handlers.AdvanceOrders(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/vendor/orders/{order_id}/decline", handlers.DeclineOrders(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/vendor/items/toggle_availability", handlers.DeclineOrders(app)).Methods("POST")
+	authenticatedRoutes.HandleFunc("/vendor/earnings-list", handlers.GetDayListEarnings(app)).Methods("POST")
 
 	//authenticatedRoutes.HandleFunc("/protected", app.protected).Methods("GET")
 
