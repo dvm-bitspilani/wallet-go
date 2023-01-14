2e463549aa0a8218158e71dfd10f1b5b9ce39cfd
Harsh Singh <harshsingh050803@gmail.com>
2023-01-14 05:34:40 +0530

    refactor: change transactionService to return a status code as well

--- service/transactionService.go
+++ service/transactionService.go
@@ -22,32 +22,37 @@ func NewTransactionOps(ctx context.Context, app *ent.Client) *TransactionOps {
 	}
 }
 
-func GenerateAndPerform(amt int, kind helpers.Txn_type, srcUser *ent.User, dstUser *ent.User, ctx context.Context, client *ent.Client) (*ent.Transactions, error) {
-	userOps := NewUserOps(ctx, client)
+//type TransactionStruct {
+//
+//}
 
+func GenerateAndPerform(amt int, kind helpers.Txn_type, srcUser *ent.User, dstUser *ent.User, ctx context.Context, client *ent.Client) (*ent.Transactions, error, int) {
+	var statusCode int
+
+	userOps := NewUserOps(ctx, client)
 	src, err := userOps.GetOrCreateWallet(srcUser)
 	if err != nil {
-		return nil, err
+		return nil, err, 403 // 403
 	}
 	dst, err := userOps.GetOrCreateWallet(dstUser)
 	if err != nil {
-		return nil, err
+		return nil, err, 403 // 403
 	}
 
 	if src == dst {
 		err := fmt.Errorf("reflexive transfers are not allowed")
 		//err := exceptions.Exception{Message: "Reflexive transfers are not allowed", Status: 403}
-		return nil, err
+		return nil, err, 403 // 403
 	}
 	if srcUser.Disabled {
 		err := fmt.Errorf("%s has been disabled", srcUser.Username)
 		//err := exceptions.Exception{Message: fmt.Sprintf("%s has been disabled", src_user.Username), Status: 403}
-		return nil, err
+		return nil, err, 403 // 403
 	}
 	if dstUser.Disabled {
 		err := fmt.Errorf("%s has been disabled", dstUser.Username)
 		//err := exceptions.Exception{Message: fmt.Sprintf("%s has been disabled", src_user.Username), Status: 403}
-		return nil, err
+		return nil, err, 403 // 403
 	}
 	occupationPair := []string{srcUser.Occupation.String(), dstUser.Occupation.String()}
 	validOccupationPair := false // It's probably a good idea to prevent any transaction than to allow *any* transaction
@@ -61,19 +66,19 @@ func GenerateAndPerform(amt int, kind helpers.Txn_type, srcUser *ent.User, dstUs
 		if !(srcUser.Username == "SWD" && dstUser.Occupation == "bitsian") {
 			err := fmt.Errorf("transaction forbidden: %s", occupationPair)
 			//err := exceptions.Exception{Message: fmt.Sprintf("Transaction forbidden: %s", occupationPair), Status: 403}
-			return nil, err
+			return nil, err, 403 // 403
 		}
 	}
 	walletOps := NewWalletOps(ctx, client)
 	if !(srcUser.Occupation == "teller") {
-		err = walletOps.Deduct(src, amt)
+		err, statusCode = walletOps.Deduct(src, amt)
 		if err != nil {
-			return nil, err
-		}
-		err = walletOps.Add(dst, amt, database.GetBalanceFromTransactionType(kind))
+			return nil, err, statusCode
+		} // 400, 412
+		err, statusCode = walletOps.Add(dst, amt, database.GetBalanceFromTransactionType(kind))
 		if err != nil {
-			return nil, err
-		}
+			return nil, err, statusCode
+		} // 400
 	}
 	return client.Transactions.Create().
 		SetUser(dstUser).
@@ -81,7 +86,7 @@ func GenerateAndPerform(amt int, kind helpers.Txn_type, srcUser *ent.User, dstUs
 		SetKind(kind).
 		SetSource(src).
 		SetDestination(dst).
-		SaveX(ctx), nil
+		SaveX(ctx), nil, 0
 }
 
 func (r *TransactionOps) ToDict(txn *ent.Transactions) map[string]string {
